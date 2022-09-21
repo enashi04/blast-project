304d2e020b10d02087509ad4e74a759daf914c97
enashi04 <ihsane.errami@gmail.com>
2022-09-21 12:24:13 +0200

    Remove some comments

--- ballast/initresfile.c
+++ ballast/initresfile.c
@@ -53,16 +53,9 @@ int initresfile(FILE *file, char *line, BlastHeader *blhd)
 */
 char *substr(const char *src, int m, int n)
 {
-    // get the length of the destination string
     int len = n - m;
-
-    // allocate (len + 1) chars for destination (+1 for extra null character)
     char *dest = (char *)malloc(sizeof(char) * (len + 1));
-
-    // start with m'th char and copy `len` chars into the destination
     strncpy(dest, (src + m), len);
-
-    // return the destination string
     return dest;
 }
 
