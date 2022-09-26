09e24d672f14938b9673fa3fd996a0f6892ca91f
enashi04 <ihsane.errami@gmail.com>
2022-09-26 15:50:12 +0200

    delete  a folder

---
+++ ballast/testing/macros.h
@@ -0,0 +1,24 @@
+/****** Maximum length for file name *******/
+#define MAXPATHLEN 132
+
+
+#define ID     2
+#define SIM    0.2
+#define RIEN   -1
+#define OUTOFHSP 0
+
+#define DEFMAXP 0.1
+
+#define MINWORD 2
+
+#define SPENALTY 99999999.0
+
+#define  ELIMIT 0.001
+
+#ifndef MIN
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#endif
+
+#ifndef MAX
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#endif
