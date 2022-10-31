813318d19677454f84b8029be399c5aabb4b46eb
enashi04 <ierrami@LAPTOP-G7JADLFM.localdomain>
2022-10-31 08:12:19 +0100

    /

--- XMLParse/version2/parsexml.c
+++ XMLParse/version2/parsexml.c
@@ -13,11 +13,60 @@ struct {
 } state;
 
 // Opening tag
-void tag_start(void *data, const char *name, const char **attrs) {
+void Hit_accession(void *data, const char *name, const char **attrs) {
 	if (strcmp(name, "Hit_accession") == 0)
 		state.access = 1;
 }
 
+void Hsp_num(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_num") == 0)
+		state.access = 1;
+}
+
+void Hsp_identity(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_identity") == 0)
+		state.access = 1;
+}
+void Hsp_evalue(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_evalue") == 0)
+		state.access = 1;
+}
+void Hsp_score(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, " Hsp_score") == 0)
+		state.access = 1;
+}
+void Hsp_gaps(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_gap") == 0)
+		state.access = 1;
+}
+void Hsp_align_len(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_align-len") == 0)
+		state.access = 1;
+}
+void Hsp_hit_from(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_hit-from") == 0)
+		state.access = 1;
+}
+void Hsp_hit_to(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_hit-to") == 0)
+		state.access = 1;
+}
+void Hsp_query_from(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_query-from") == 0)
+		state.access = 1;
+}
+void Hsp_query_to(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_query-to") == 0)
+		state.access = 1;
+}
+void Hsp_positive(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, " Hsp_positive") == 0)
+		state.access = 1;
+}
+void Hsp_bitscore(void *data, const char *name, const char **attrs) {
+	if (strcmp(name, "Hsp_bitscore") == 0)
+		state.access = 1;
+}
 // Closing tag
 void tag_end(void *data, const char *name) {
 }
@@ -28,7 +77,18 @@ void tag_value(void *data, const char *text, int len) {
 	content[len] = '\0';
 	
 	if (state.access) {
-		printf("accession id: %s\n", content);
+		printf("%s,\n", content);
+		strcpy(content, state.query);
+		state.access = 0;
+	}
+}
+
+void tag_value_aln(void *data, const char *text, int len) {
+	strncpy(content, text, len);
+	content[len] = '\0';
+	
+	if (state.access) {
+		printf("%s\n", content);
 		strcpy(content, state.query);
 		state.access = 0;
 	}
@@ -50,7 +110,7 @@ int main(int argc, char *argv[]) {
 			exit(1);
 		}
 		XML_Parser parser = XML_ParserCreate(NULL);
-		XML_SetElementHandler(parser, tag_start, tag_end);
+		XML_SetElementHandler(parser, Hit_accession, tag_end);
 		XML_SetCharacterDataHandler(parser, tag_value);
 
 		state.access = 0;
@@ -81,8 +141,41 @@ int main(int argc, char *argv[]) {
 			exit(1);
 		}
 		XML_Parser parser = XML_ParserCreate(NULL);
-		XML_SetElementHandler(parser, tag_start, tag_end);
+		XML_SetElementHandler(parser, Hit_accession, tag_end);
 		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_evalue, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_score, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_bitscore, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_query_from, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_query_to, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_hit_from, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_hit_to, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_positive, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_identity, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser, Hsp_gaps, tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value);
+
+		XML_SetElementHandler(parser,Hsp_align_len,tag_end);
+		XML_SetCharacterDataHandler(parser, tag_value_aln);
 
 		state.access = 0;
 
