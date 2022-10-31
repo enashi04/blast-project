7c43c23db0b88a95155506080c4a371e04703fff
enashi04 <ihsane.errami@gmail.com>
2022-10-31 10:32:59 +0100

    add comments

--- XMLParse/version2/parsexml.c
+++ XMLParse/version2/parsexml.c
@@ -13,6 +13,11 @@ struct {
 	char access;
 } state;
 
+/** @brief Open the tag for the Bronze Mode
+    @param data 
+    @param name 
+    @param attrs 
+*/
 void bronze_tag_start(void *data, const char *name, const char **attrs){
 	if (strcmp(name, "Hit_accession") == 0){
 		state.access = 1;
@@ -31,6 +36,11 @@ void bronze_tag_start(void *data, const char *name, const char **attrs){
 	}
 }
 
+/** @brief Open the tag for the Silver Mode
+    @param data 
+    @param name 
+    @param attrs 
+*/
 void silver_tag_start(void *data, const char *name, const char **attrs){
 	if (strcmp(name, "Hit_accession") == 0){
 		state.access = 1;
@@ -57,7 +67,11 @@ void silver_tag_start(void *data, const char *name, const char **attrs){
 		state.access = 2;
 	}
 }
-// Opening tag
+/** @brief Open the tag for the Gold Mode
+    @param data 
+    @param name 
+    @param attrs 
+*/
 void gold_tag_start(void *data, const char *name, const char **attrs) {
 	if (strcmp(name, "Hit_accession") == 0){
 		state.access = 1;
@@ -101,11 +115,19 @@ void gold_tag_start(void *data, const char *name, const char **attrs) {
 	
 }
 
-// Closing tag
+/** @brief Closing the tag
+    @param data 
+    @param name 
+*/
 void tag_end(void *data, const char *name) {
 }
 
-// Reading tag content
+
+/** @brief Read the tag content
+    @param data 
+    @param text 
+    @param len 
+*/
 void tag_value(void *data, const char *text, int len) {
 	strncpy(content, text, len);
 	content[len] = '\0';
@@ -122,6 +144,10 @@ void tag_value(void *data, const char *text, int len) {
 	}
 }
 
+/** @brief display errors and close file f
+    @param f 
+    @param parser 
+*/
 void test_error(FILE *f, XML_Parser parser){
 	int done;
 	do {
@@ -144,6 +170,9 @@ void test_error(FILE *f, XML_Parser parser){
 }
 
 int main(int argc, char *argv[]) {
+/**************************************************************************************/
+/*********************************More than 2 args*************************************/
+/**************************************************************************************/
 	if (argc > 3) {
 		printf("You need to put 0 or 2 arguments\n");
 		printf("0 argument: The program will parse stdin.xml on the Gold mode\n");
@@ -152,20 +181,33 @@ int main(int argc, char *argv[]) {
 		exit(1);
 
 	}
+/**************************************************************************************/
+/**************************************2 args******************************************/
+/**************************************************************************************/
 	else if(argc==3){
 		FILE *f = fopen(argv[1], "r");
+/**************************************************************************************/
+/*************************************File empty***************************************/
+/**************************************************************************************/
 		if (!f) {
 			fprintf(stderr, "Can't open blast file\n");
 			exit(1);
 		}
+/**************************************************************************************/
+/********************************2nd arg not an integer********************************/
+/**************************************************************************************/
 		if(atoi(argv[2])!=1 && atoi(argv[2])!= 2 && atoi(argv[2])!=3){
 			fprintf(stderr, "arg2 : Mode choice: 1 (Bronze) | 2 (Silver) | 3 (Gold)\n");
 			exit(1);
 		}
-		printf("On commence le parsing du document\n");
+
 		char *filename= (char *)malloc(100);
 		XML_Parser parser ;
+
 		switch(atoi(argv[2])){
+/**************************************************************************************/
+/***********************************Bronze mode****************************************/
+/**************************************************************************************/
 			case 1:
 				printf("on passe ici?");
 				strcpy(filename, "../BronzeResults_");
@@ -176,8 +218,10 @@ int main(int argc, char *argv[]) {
 
 				state.access = 0;
 				test_error(f, parser);
-		
 				break;
+/**************************************************************************************/
+/**********************************Silver mode*****************************************/
+/**************************************************************************************/
 			case 2 :
 				strcpy(filename, "../SilverResults_");
 				strcat(strcat(filename,argv[1]), ".csv");
@@ -189,6 +233,9 @@ int main(int argc, char *argv[]) {
 				test_error(f, parser);
 		
 				break;
+/**************************************************************************************/
+/************************************Gold Mode*****************************************/
+/**************************************************************************************/
 			case 3:
 				strcpy(filename, "../GoldResults_");
 				strcat(strcat(filename,argv[1]), ".csv");
@@ -202,14 +249,23 @@ int main(int argc, char *argv[]) {
 				break;
 			default : break;
 		}
-		
 	}
+/**************************************************************************************/
+/************************************0 argument****************************************/
+/**************************************************************************************/
 	else if(argc ==1){
+/**************************************************************************************/
+/**************************We're gonna open the example file***************************/
+/**************************************************************************************/
 		FILE *f = fopen("stdin.xml", "r");
 		if (!f) {
 			fprintf(stderr, "Can't open blast file\n");
 			exit(1);
 		}
+/**************************************************************************************/
+/************************************Gold Mode*****************************************/
+/**************************************************************************************/
+		fprintf(stderr, "We're gonna display the Goldresults of stdin.xml");
 		XML_Parser parser = XML_ParserCreate(NULL);
 		XML_SetElementHandler(parser, gold_tag_start, tag_end);
 		XML_SetCharacterDataHandler(parser, tag_value);
