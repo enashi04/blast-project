4ada2de4bde5febc6ac123b4308a400f738ba85c
enashi04 <ierrami@LAPTOP-G7JADLFM.localdomain>
2022-11-14 14:17:52 +0100

    Little modif parser part 2

--- XMLParse/version2/sc/options.h
+++ XMLParse/version2/sc/options.h
@@ -10,15 +10,66 @@ void help(){
     fprintf(stderr,"\r -h \t Show this screen.\n");
     fprintf(stderr,"\r -m \t Choose mode [bronze, silver or gold].\n");
     fprintf(stderr,"\r -o \t Put an output file\n");
-    fprintf(stderr,"\r -a \t Display CSV file example\n");
-    exit(1);
+    fprintf(stderr,"\r -a \t Display CSV file example\n\n");
+    
 };
 
 //si y'a la présence du mode alors il faut qu'on voit l'argument d'apres. Si c'est bronze argent ou or.
-void mode();
-
-//si l'argument est -d, alors on affiche directement un exemple de fichier parsé sous le format gold
-void display();
+char* modeChoice(char *argv) {
+    if(strcmp("bronze", argv)==0){
+        return argv;
+    }
+    else if(strcmp("silver", argv)==0){
+        return argv;
+    }
+    else if(strcmp("gold", argv)==0){
+        return argv;
+    }
+    else{
+        fprintf(stderr,"The mode argument is not valid.\n");
+        fprintf(stderr,"Please choose a mode between : bronze, silver and gold;\n");
+        exit(1);
+    }
+};
 
  //fichier de sortie si y'a un -o
-void output();
+char *outputName(char *argv){
+    if(strstr(argv, ".csv")!=NULL){
+        return argv;
+    }
+    else{
+        fprintf(stderr,"The output name is not valid. You need to add the csv extension\n");
+        fprintf(stderr,"Like output.csv\n");
+        exit(1);
+    }
+};
+
+void getArgs(int argc, char **argv, char *mode, char *output, int displayConsole){
+    for (int i = 0; i < argc; i++)
+    {
+        if (strncmp(argv[i], "-m", 2) == 0)
+        {
+            // faut que ca retourne un char ici pour que le mode soit choisi;
+            mode = modeChoice(argv[i + 1]);
+        }
+        if (strncmp(argv[i], "-o", 2) == 0)
+        {
+            // récupération de l'output
+            output = outputName(argv[i + 1]);
+        }
+        if (strncmp(argv[i], "-a", 2) == 0)
+        {
+            // affichage du résultat sur la console.
+            displayConsole = 1;
+        }
+        if(strncmp(argv[i], "-h", 2)==0 || strncmp(argv[i], "help", 4)==0 ){
+            help();
+            if(strncmp(argv[i+1], "-m", 2) != 0 || strncmp(argv[i+1], "-o", 2) != 0 || strncmp(argv[i+1], "-c", 2) != 0){
+                fprintf(stderr,"Error : do not know -> %s\n", argv[i+1]);
+            }
+            exit(1);
+        }
+        
+
+    }
+};
--- XMLParse/version2/sc/parsingv2.c
+++ XMLParse/version2/sc/parsingv2.c
@@ -7,7 +7,7 @@
 
 char buf[BUFSIZE];
 char content[BUFSIZE];
-//FILE *output;
+FILE *output;
 //int mode;
 
 
@@ -18,6 +18,238 @@ struct
 } state;
 
 
+/** @brief Open the tag for the Bronze Mode
+    @param data
+    @param name
+    @param attrs
+*/
+void bronze_tag_start(void *data, const char *name, const char **attrs)
+{
+    if (strcmp(name, "Hit_accession") == 0)
+    {
+        state.access = 5;
+    }
+    if (strcmp(name, "Hsp_num") == 0)
+    {
+        state.access = 6;
+    }
+    if (strcmp(name, "Hsp_score") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_evalue") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_align-len") == 0)
+    {
+        state.access = 2;
+    }
+    // if (strcmp(name, "Iteration_query-def") == 0)
+    // {
+    //     state.access = 3;
+    // }
+    // if (strcmp(name, "Iteration_query-len") == 0)
+    // {
+    //     state.access = 4;
+    // }
+}
+
+/** @brief Open the tag for the Silver Mode
+    @param data
+    @param name
+    @param attrs
+*/
+void silver_tag_start(void *data, const char *name, const char **attrs)
+{
+
+    if (strcmp(name, "Hit_accession") == 0)
+    {
+        state.access = 5;
+    }
+    if (strcmp(name, "Hsp_num") == 0)
+    {
+        state.access = 6;
+    }
+    if (strcmp(name, "Hsp_score") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_evalue") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_identity") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_positive") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_gaps") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_align-len") == 0)
+    {
+        state.access = 2;
+    }
+    // if (strcmp(name, "Iteration_query-def") == 0)
+    // {
+    //     state.access = 3;
+    // }
+    // if (strcmp(name, "Iteration_query-len") == 0)
+    // {
+    //     state.access = 4;
+    // }
+}
+/** @brief Open the tag for the Gold Mode
+    @param data
+    @param name
+    @param attrs
+*/
+void gold_tag_start(void *data, const char *name, const char **attrs)
+{
+
+    if (strcmp(name, "Hit_accession") == 0)
+    {
+        state.access = 5;
+    }
+    if (strcmp(name, "Hsp_num") == 0)
+    {
+        state.access = 6;
+    }
+    if (strcmp(name, "Hsp_bit-score") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_score") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_evalue") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_query-from") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_query-to") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_hit-from") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_hit-to") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_identity") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_positive") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_gaps") == 0)
+    {
+        state.access = 1;
+    }
+    if (strcmp(name, "Hsp_align-len") == 0)
+    {
+        state.access = 2;
+    }
+}
+
+/** @brief Closing the tag
+    @param data
+    @param name
+*/
+void tag_end(void *data, const char *name)
+{
+}
+
+/** @brief Read the tag content
+    @param data
+    @param text
+    @param len
+*/
+void tag_value(void *data, const char *text, int len)
+{
+    strncpy(content, text, len);
+    content[len] = '\0';
+
+    if (state.access == 1)
+    {
+        fprintf(output, "%s,", content);
+        strcpy(content, state.query);
+        state.access = 0;
+    }
+    if (state.access == 2)
+    {
+        fprintf(output, "%s\n", content);
+        strcpy(content, state.query);
+        state.access = 0;
+    }
+  
+    if (state.access == 5) // pour le nom de l'hit
+    {
+        fprintf(output, "%s,", content);
+        strcpy(content, state.query);
+        state.access = 0;
+    }
+    if (state.access == 6) // pour le numéro du hit
+    {
+        if (strncmp(content, "1", 1) == 0)
+        {
+            fprintf(output, "%s,", content);
+            strcpy(content, state.query);
+            state.access = 0;
+        }
+        else
+        {
+            fprintf(output, "-,%s,", content);
+            strcpy(content, state.query);
+            state.access = 0;
+        }
+    }
+}
+
+/** @brief display errors and close file f
+    @param f
+    @param parser
+*/
+void test_error(FILE *f, XML_Parser parser)
+{
+    int done;
+    do
+    {
+        int len = fread(buf, 1, BUFSIZE, f);
+
+        if (ferror(f))
+        {
+            fprintf(stderr, "Read error\n");
+            break;
+        }
+        done = feof(f);
+
+        if (XML_Parse(parser, buf, len, done) == XML_STATUS_ERROR)
+        {
+            fprintf(stderr, "Error while parsing XML\n");
+            break;
+        }
+    } while (!done);
+
+    XML_ParserFree(parser);
+    fclose(f);
+}
+
+
 int main(int argc, char **argv){
     if (argc==1)
     {
@@ -26,26 +258,15 @@ int main(int argc, char **argv){
         fprintf(stderr, "-h or help to see options\n");
         exit(1);
     }
-    else{
-        for(int i =0; i<argc; i++){
-            if(strncmp(argv[i], "-h", 2)==0 || strncmp(argv[i], "help", 4)==0 ){
-              help();
-            }
-            if(strncmp(argv[i], "-m", 2)==0){
-                //faut que ca retourne un char ici pour que le mode soit choisi;
-            }
-            if(strncmp(argv[i], "-o", 2)==0){
-                //récupération de l'output
-            }
-            if(strncmp(argv[i],"-d", 2)==0){
-                //affichage d'un exemple de csv
-            }
-            if(strncmp(argv[i], "-c", 2)==0){
-                //affichage du résultat sur la console.
-            }
+    char *name=NULL;
+    char *outName = NULL;
+    int displayConsole = 0;
+    getArgs(argc, argv,name, outName, displayConsole);
 
-            //printf("l'argument est %u %s\n", i, argv[i]);
-        }
-    }
+    // Affichage du résultat en fonction du mode
+
+    //affichage du résultat sur la console
+
+    //affichage d'un exemple CSV
     return 0;
 }
