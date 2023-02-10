275bb20d5d3c08edc11e987361dcb83ad450906b
enashi04 <ihsane.errami@gmail.com>
2023-02-10 12:59:37 +0100

    remove old repository

--- XMLParse/PARSERXML/ballastParser.c
+++
@@ -1,5 +0,0 @@
-//ici méthode pour récupérer les motifs de ballast 
-
-//trouver un moyen de récupérer les motifs à partir des séquences données en xml et le mettre dans ballast
-
-//-> prendre en compte les autres formats d'entrée !
--- XMLParse/newVersion/testing/lineage.h
+++
@@ -1,8 +0,0 @@
-#ifndef LINEAGE_H
-#define LINEAGE_H
-#include "parameters.h"
-char *makebuffer(char *filename);
-char *getParent(SpeciesInfo *speciesInfo, char *id);
-char *getLineage(SpeciesInfo *speciesInfo, char *id, char lineage[MAX_SIZE], char *parentTarget);
-char *createLineage(SpeciesInfo *speciesInfo,char *id_parent);
-#endif
