013e607810cde19debcef65a0c652ea60b2efe4b
enashi04 <ihsane.errami@gmail.com>
2023-02-21 11:17:06 +0100

    edit parameters.h : add lineage

--- XMLParse/newVersion/parameters.h
+++ XMLParse/newVersion/parameters.h
@@ -1,19 +1,22 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
-#define MIN_SIZE 128
-#define MAX_SIZE 4096
-#define MAXI_SIZE 8192
-#define FILE_SIZE 2429416
+#define MIN_SIZE 128 //taille minimal
+#define MAX_SIZE 4096 //taille 
+#define MAXI_SIZE 8192 //taille maximal pour la lignée
+#define FILE_SIZE 2429416 //taille du fichier taxonomy.dat
+#define EXTRA_SIZE 16777216 //nombre maximal de caractère dans le fichier de sortie 2^24
 
-#define FICHIER "taxonomy.dat"
+#define FICHIER "taxonomy.dat" //fichier taxonomy
 
-//définir une structure
+//structure contenant les infos de l'espèce trouvé
 typedef struct SpeciesInfo {
     char *name;
     char *rank;
     char *id;//indice du parent 
     char *id_parent;//int
+    //ajout de la lignée 
+    char *lineage;
 }SpeciesInfo;
 
 #endif
