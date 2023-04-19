9038b5b200856a8bd377471f56d30776e2460079
ihsan <ihsane.errami@etu.unistra.fr>
2023-04-19 13:20:33 +0200

    edit hashmap and retrieveInfo to get correctly the lineage

--- XMLParse/newVersion/hashmap.c
+++ XMLParse/newVersion/hashmap.c
@@ -4,20 +4,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-// fonction de hachage basique pour les chaînes de caractères
 unsigned int hash(const char *key)
 {
     unsigned int hash = 0;
     for (int i = 0; key[i] != '\0'; i++)
     {
-        hash = hash * 31 + key[i]; // La multiplication par 31 est utilisée car peut être implémentée efficacement à l'aide d'un simple décalage de bits.
+        if(key[i] == ','){
+            break;
+        }
+        // La multiplication par 31 est utilisée car peut être implémentée efficacement à l'aide d'un simple décalage de bits.
+        hash = hash * 31 + key[i]; 
     }
     return hash % SPECIES_SIZE;
 }
+
 // fonction pour insérer une nouvelle paire clé-valeur dans la hashmap
 void insert(Hashmap *hashmap, const char *key, int value)
 {
-    
     // calculer l'index dans le tableau en utilisant la fonction de hachage
     unsigned int index = hash(key);
     // récupérer l'élément correspondant dans le tableau
@@ -39,20 +42,61 @@ void insert(Hashmap *hashmap, const char *key, int value)
     new_entry->value = value;
     new_entry->next = hashmap->entries[index];
     hashmap->entries[index] = new_entry;
+    
+}
+
+void insert_othername(Hashmap *hashmap, const char *name, const char *value){
+    //parcourir la chaine de caractère name
+    int len = strlen(name);
+    char *new_name;
+    for(int i = 0; i < len; i++){
+        if(name[i] == ',' && name[i+1]==' '){
+            new_name = (char *)malloc((i+1) * sizeof(char));
+            for(int j = 0; j < i; j++){
+                new_name[j] = name[j];
+            }
+            new_name[i] = '\0';
+            insert(hashmap, new_name, atoi(value));
+            free(new_name);
+        }
+    }
 }
 
 // fonction pour créer une nouvelle hashmap
 Hashmap *createHashMap(char *buffer)
 {
     Hashmap *hashmap = (Hashmap *)malloc(sizeof(Hashmap));
-    char id_species[MIN_SIZE], name_species[MAX_SIZE];
+    char id_species[MIN_SIZE], name_species[MAX_SIZE], othername_species[MAXI_SIZE];
     char *line = strtok(strdup(buffer), "\n");
     line = strtok(NULL, "\n");
-
     while (line != NULL)
     {
         sscanf(line, "%[^	]	%*[^	]	%[^	]	%*[^\n]", id_species, name_species);
-     
+        int len = strlen(line), iteration=0;
+        // Boucle pour récupérer le other name
+        for (int i = 0; i < len; i++)
+        {
+            if(line[i]=='\t'){
+                iteration ++;
+                if(iteration==5){
+                    int j=0;
+                    for(int k=i+1; k<len; k++){
+                        if (line[k] == '\t')
+                        {
+                            othername_species[j] = '\0';
+                            //faire appel à une autre fonction ici 
+                            insert_othername(hashmap, othername_species, id_species);
+                            break;
+                        }
+                        else
+                        {
+                            othername_species[j] = line[k];
+                        }
+                        j++;
+                    }
+                }
+            }  
+        }
         insert(hashmap, name_species, atoi(id_species));
         line = strtok(NULL, "\n");
     }
@@ -62,20 +106,24 @@ Hashmap *createHashMap(char *buffer)
 // fonction pour récupérer la valeur associée à une clé donnée
 int get(Hashmap *hashmap, const char *key)
 {
-     // calculate the index in the array using the hash function
     unsigned int index = hash(key);
-    // get the corresponding element in the array
     Entry *entry = hashmap->entries[index];
-    // traverse the linked list to find the element corresponding to the given key
     while (entry != NULL)
     {
         if (strcmp(entry->key, key) == 0)
         {
-            // if the corresponding element is found, return its value
             return entry->value;
         }
         entry = entry->next;
-    }
-    // if the corresponding element is not found, return -1
+     }
     return -1;
 }
+
+// int main(int argc, char **argv){
+//     // // créer la hashmap
+//     Hashmap *hashmap = createHashMap(makebuffer("taxonomy.dat"));
+//     // récupérer la valeur de Anser cygnoides
+//     int value = get(hashmap, "Anser cygnoides");
+//     printf("Anser cygnoides: %d\n", value);
+ 
+// } 
--- XMLParse/newVersion/retrieveInfo.c
+++ XMLParse/newVersion/retrieveInfo.c
@@ -27,18 +27,18 @@ void blastOutPut_iteration(xmlDoc *fichier, char *mode, char *buffer, char tabIn
 
     char speciesName[MIN_SIZE];
     int query_length = 0;
-
+    //get the blast version and the database
     for (node = child; node; node = node->next)
     {
         blastVersion(node);
         blastDB(node);
     }  
-    //remplissage de la structure speciesInfo et intialisation de la hashmap
+    //fill the structure and create the HashMap
     SpeciesInfo *speciesInfo = fillStructure(buffer);
     FillSpeciesInfo *fillInfo = (FillSpeciesInfo *)malloc(sizeof(FillSpeciesInfo)*MAXI_SIZE);
     Hashmap *hashmap = createHashMap(buffer);
 
-   //mode gold : charger
+   //condition for gold mode : convert the xml to blastp file
     if(strcmp(mode, "gold")==0){
         char *database = (char*)malloc(sizeof(char));
         char *blastInfo=getInfoBlast(child, database);
@@ -66,31 +66,33 @@ void blastOutPut_iteration(xmlDoc *fichier, char *mode, char *buffer, char tabIn
                 {
                     // ITERATION SUBNODES
                     char *iteration_num;
-                    //fonction pour récupérer les infos de la query
-                    //getQueryInfo(child, speciesName, query_length, iteration_num);  
                     for(xmlNode *childnode=child->children; childnode; childnode=childnode->next){
+                        // get the query def
                         if(strcmp("Iteration_query-def", (const char *)childnode->name) == 0)
                         {
                             char *query_def = (char *)xmlNodeGetContent(childnode);
                             strcpy(speciesName, query_def);
                             fprintf(output, " \t{\n\t\t\"query-name\" : \"%s\",\n", query_def);
                         }
+                        // get the iteration number
                         else if(strcmp("Iteration_iter-num", (const char *)childnode->name) == 0){
                             iteration_num=(char *)xmlNodeGetContent(childnode);
                         }
+                        // get the query length
                         else if (strcmp("Iteration_query-len", (const char *)childnode->name) == 0)
                         {
                             fprintf(output, "\t\t\"query-length\" : \"%s\",\n", xmlNodeGetContent(childnode));
                             query_length = atoi((const char *)xmlNodeGetContent(childnode));
                         }
                     }
-                    //printf("iteration %s, length %u, species %s", iteration_num, query_length, speciesName); 
+                    //display the species name
                     displayQuerySpecies(speciesName);
                     node_Iteration(child, mode, speciesInfo, query_length, fillInfo, hashmap,tabInfo, iteration_num); //ajout de la table d'information
                 }
             }
         }
     }
+    //free the memory
     free(hashmap);
     free(fillInfo);
     free(speciesInfo);
@@ -277,12 +279,12 @@ void node_HSP(xmlNode *node, char *mode,int query_length, SpeciesInfo *speciesIn
 {
     char *fragment ="no";
     char *hit_id = getHitAccession(node),*species = getSpecies(node, fragment);
-    // CHILD = SOUS-NOEUD DU NODE
     xmlNode *child = node->children;
-    const char *name = "Hit_hsps", *name2 = "Hsp";
+    const char *nameHitHspNode = "Hit_hsps", *nameHspNode = "Hsp";
+
     for (node = child; node; node = node->next)
     {
-        if (strcmp(name, (const char *)node->name) == 0)
+        if (strcmp(nameHitHspNode, (const char *)node->name) == 0)
         {
             // CHILDNODE = SOUS-NOEUD DE NODE
             xmlNode *childNode = node->children;
@@ -290,43 +292,36 @@ void node_HSP(xmlNode *node, char *mode,int query_length, SpeciesInfo *speciesIn
             {
                 if(strcmp((char *)child->name, "text")!=0){
                 // NOEUD DANS LEQUEL ON SE TROUVE = HSP
-                    if (strcmp(name2, (char *)child->name) == 0)
+                    if (strcmp(nameHspNode, (char *)child->name) == 0)
                     {
                         if (strcmp(name_hit, "") == 0)
                         {
                             strcpy(name_hit, hit_id);
                         }
-                       fprintf(output, "\t\t\t{\n\t\t\t\t\"hit_accession\" : \"%s\",\n", hit_id);
-                       fprintf(output, "\t\t\t\t\"fragment\" : \"%s\",\n", fragment);
-                        //pour l'affichace de l'espèce
-                        fprintf(output, "\t\t\t\t\"species\" : {\n");
+                       fprintf(output, "\t\t\t{\n\t\t\t\t\"hit_accession\" : \"%s\",\n\t\t\t\t\"fragment\" : \"%s\",\n\t\t\t\t\"species\" : {\n", hit_id, fragment);
+
+                        //faire appel à la hashmap pour récupérer l'id de l'espèce
+                        int taxId = get(hashmap, species);
                         //1e ajout dans la deuxieme structure
                         if(size_struct==0){
-                            for (int i = 0; i < SPECIES_SIZE - 1; i++)
-                            {
-                                if (strcmp(speciesInfo[i].name, species) == 0)
-                                {
-                                    fillInfo[0].name=speciesInfo[i].name;
-                                    fillInfo[0].id=i;
-                                    fillInfo[0].rank=speciesInfo[i].rank;
-                                    
-                                    fprintf(output, "\t\t\t\t\t\"name\":\"%s\",\n",fillInfo[0].name);
-                                    fprintf(output, "\t\t\t\t\t\"taxid\":\"%u\",\n", fillInfo[0].id);
-                                    fprintf(output,"\t\t\t\t\t\"rank\":\"%s\"",fillInfo[0].rank);
-                                    if(strcmp(tabInfo[0][1], "1")==0){
-                                        fillInfo[0].lineage=createLineage(speciesInfo, species, hashmap);
-                                        fprintf(output,",\n%s\n",fillInfo[0].lineage);
-                                    }
-                                    size_struct=1;
-                                    break;
-                                }
+                            fillInfo[0].name=speciesInfo[taxId].name;
+                            fillInfo[0].id=taxId;
+                            fillInfo[0].rank=speciesInfo[taxId].rank;
+                            
+                            fprintf(output, "\t\t\t\t\t\"name\":\"%s\",\n",fillInfo[0].name);
+                            fprintf(output, "\t\t\t\t\t\"taxid\":\"%u\",\n", fillInfo[0].id);
+                            fprintf(output,"\t\t\t\t\t\"rank\":\"%s\"",fillInfo[0].rank);
+                            if(strcmp(tabInfo[0][1], "1")==0){
+                                fillInfo[0].lineage=createLineage(speciesInfo, species, hashmap);
+                                fprintf(output,",\n%s\n",fillInfo[0].lineage);
                             }
+                            size_struct=1;
                         }
                         //parcours de la deuxieme structure
-                    else if(size_struct>0){
+                        else if(size_struct>0){
                             int check=0;
                             for(int i=0; i<size_struct; i++){
-                                if(strcmp(fillInfo[i].name,species)==0){
+                                if(fillInfo[i].id==taxId){
                                     fprintf(output, "\t\t\t\t\t\"name\":\"%s\",\n",fillInfo[i].name);
                                     fprintf(output, "\t\t\t\t\t\"taxid\":\"%u\",\n",fillInfo[i].id);
                                     fprintf(output,"\t\t\t\t\t\"rank\":\"%s\"",fillInfo[i].rank);
@@ -339,13 +334,13 @@ void node_HSP(xmlNode *node, char *mode,int query_length, SpeciesInfo *speciesIn
                             }
                             if(check == 0){
                                 //on ajoute les infos à la position size_struct et on modifie la valeur de size_struct
-                                for (int i = 0; i < SPECIES_SIZE - 1; i++)
-                                {
-                                    if (strcmp(speciesInfo[i].name, species) == 0)
+                                if(taxId!=-1){
+                                    for (int i = 0; i < SPECIES_SIZE - 1; i++)
                                     {
-                                        fillInfo[size_struct].name=speciesInfo[i].name;
-                                        fillInfo[size_struct].id=i;
-                                        fillInfo[size_struct].rank=speciesInfo[i].rank;
+                                        printf("le taxid est %d et species %s\n", taxId, species);
+                                        fillInfo[size_struct].name=speciesInfo[taxId].name;
+                                        fillInfo[size_struct].id=taxId;
+                                        fillInfo[size_struct].rank=speciesInfo[taxId].rank;
 
                                         fprintf(output, "\t\t\t\t\t\"name\":\"%s\",\n",fillInfo[size_struct].name);
                                         fprintf(output, "\t\t\t\t\t\"taxid\":\"%u\",\n", fillInfo[size_struct].id);
@@ -357,6 +352,10 @@ void node_HSP(xmlNode *node, char *mode,int query_length, SpeciesInfo *speciesIn
                                         size_struct++;
                                         break;
                                     }
+                                }
+                                else{
+                                    //on n'affiche que le nom de l'espèce
+                                    fprintf(output, "\t\t\t\t\t\"name\":\"%s\"",species);
                                 }
                             }
                         }
