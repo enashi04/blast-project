7c66a0b45fe05848b30e514d4cb6649cc74ab653
ihsan <ihsane.errami@etu.unistra.fr>
2023-04-20 14:46:03 +0200

    add log inside parsing.c

--- XMLParse/newVersion/parsing.c
+++ XMLParse/newVersion/parsing.c
@@ -4,6 +4,7 @@
 #include "XMLtoBLASTP.h"
 #include "parameters.h"
 #include "extractMotifs.h"
+#include "log.h"
 #include "unistd.h"
 #include <sys/wait.h>
 
@@ -35,8 +36,8 @@ int main(int argc, char **argv){
     }
 
     char *buffer = makebuffer(FICHIER);
-    //création d'un tableau
-    char tabInfo[13][2][20];
+    //création d'un tableau pour le mode silver
+    char tabInfo[14][2][20];
     strcpy(tabInfo[0][0], "Lineage");
     strcpy(tabInfo[1][0], "Hsp_num");
     strcpy(tabInfo[2][0], "Hsp_identity");
@@ -50,10 +51,11 @@ int main(int argc, char **argv){
     strcpy(tabInfo[10][0], "Hsp_evalue");
     strcpy(tabInfo[11][0], "Hsp_score");
     strcpy(tabInfo[12][0], "Hsp_bit-score");
+    strcpy(tabInfo[13][0], "Query_cover");
 
- 
     if(strcmp(mode, "silver")==0){
-        for(int i=0; i<13; i++){
+        printf("Which informations do you want to display?\n");
+        for(int i=0; i<14; i++){
             char answer[2];
             printf("%s ? (y or n)", tabInfo[i][0]);
             scanf("%s", answer);
@@ -64,8 +66,14 @@ int main(int argc, char **argv){
                 strcpy(tabInfo[i][1],"0");
             }
         }
+        INFO("The following elements will be displayed:");
+        for(int i=0; i<14; i++){
+            if(strcmp(tabInfo[i][1],"1")==0){
+                INFO("%s", tabInfo[i][0]);
+            }
+        }
     }else{
-        for(int i=0; i<13; i++){
+        for(int i=0; i<14; i++){
             strcpy(tabInfo[i][1],"1");  
         }
     }
